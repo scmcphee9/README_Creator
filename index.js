7c0c46e9890ef68292ad3de4f4a381f13d690816
Shawn McPhee <scmcphee9@gmail.com>
2020-11-16 20:33:21 -0700

    installed npm inquirer, added a couple of question, wrote writetofile function and init function

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const util = require("util");
+
 // array of questions for user
 const questions = [
+  {
+    type: "input",
+    message: "What is your name?",
+    name: "name",
+  },
 
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is your project description?",
+    name: "description",
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  const fileName = `${data.title.toLowerCase()}.md`;
+  fs.writeFile(fileName, generateMarkdown(data), () => console.log(success));
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions).then((data) => {
+    return writeToFile(fileName, data);
+  });
 }
 
 // function call to initialize program
