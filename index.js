4bf06e4dbcb9a4ece5b72474d8d5de20affd6b3c
Shawn McPhee <scmcphee9@gmail.com>
2020-11-18 21:42:00 -0700

    made input questions and layout for readme file

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown");
-const util = require("util");
+// const util = require("util");
 
 // array of questions for user
 const questions = [
   {
     type: "input",
-    message: "What is your name?",
-    name: "name",
+    message: "What is your GitHub username?",
+    name: "username",
   },
 
   {
     type: "input",
-    message: "What is your project title?",
+    message: "What is your email address?",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "What is your project's name?",
     name: "title",
   },
   {
     type: "input",
-    message: "What is your project description?",
+    message: "Write a short description of your project.",
     name: "description",
+  },
+  {
+    type: "list",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "BSD 3", "GLP 3.0", "None"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "What command should be run to install dependencies?",
+    name: "dependencies",
+  },
+  {
+    type: "input",
+    message: "What command should be run to run tests",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about using the repo?",
+    name: "usingRepo",
+  },
+  {
+    type: "input",
+    message: "What does the user need to know about contributing to the repo?",
+    name: "contributingRepo",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-  const fileName = `${data.title.toLowerCase()}.md`;
-  fs.writeFile(fileName, generateMarkdown(data), () => console.log(success));
+function writeToFile(data) {
+  // const fileName = `${data.title.toLowerCase()}.md`;
+  fs.writeFile("README.md", generateMarkdown(data), () => console.log(success));
 }
 
 // function to initialize program
---
+++ layoutReadme.md
@@ -0,0 +1,45 @@
+# `${data.title}`
+
+## Project Description
+
+`${data.description}`
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [License](#license)
+- [Badges](#badges)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+- `${data.dependencies}`
+
+## Usage
+
+- `${data.usingRepo}`
+
+## Credits
+
+## Contributing
+
+- `${data.contributingRepo}`
+
+## License
+
+- `${data.license}`
+
+## Badges
+
+## Tests
+
+- `${data.tests}`
+
+## Questions
+
+- `${data.username}`
+- `${data.email}`
